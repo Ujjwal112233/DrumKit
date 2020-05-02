9a6a590a78f7b3b3bf14d51e8ed2b1a4902f527c
Ujjwal112233 <63549707+Ujjwal112233@users.noreply.github.com>
2020-05-03 01:24:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,37 +29,37 @@ function makeSound(key) {
   // }
   switch (key) {
     case "w":
-      var audio = new Audio("sounds/tom-1.mp3"); //jaise har jagah audio.play() likha h, we could also write tom1.play(sabka alag name)
+      var audio = new Audio("tom-1.mp3"); //jaise har jagah audio.play() likha h, we could also write tom1.play(sabka alag name)
       audio.play();
       break;
 
     case "a":
-      var audio = new Audio("sounds/tom-2.mp3");
+      var audio = new Audio("tom-2.mp3");
       audio.play();
       break;
 
     case "s":
-      var audio = new Audio("sounds/tom-3.mp3");
+      var audio = new Audio("tom-3.mp3");
       audio.play();
       break;
 
     case "d":
-      var audio = new Audio("sounds/tom-4.mp3");
+      var audio = new Audio("tom-4.mp3");
       audio.play();
       break;
 
     case "j":
-      var audio = new Audio("sounds/snare.mp3");
+      var audio = new Audio("snare.mp3");
       audio.play();
       break;
 
     case "k":
-      var audio = new Audio("sounds/crash.mp3");
+      var audio = new Audio("crash.mp3");
       audio.play();
       break;
 
     case "l":
-      var audio = new Audio("sounds/kick-bass.mp3");
+      var audio = new Audio("kick-bass.mp3");
       audio.play();
       break;
 
